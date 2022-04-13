d99cecc052a3a8f7d7212dfdf5762b4693599b53
Henrique Aleixo Mendes <89947763+ShadowYoung09@users.noreply.github.com>
2022-04-13 12:22:59 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function maratonKenzie(firstPlace, secondPlace, lastPlace){
+    const posicao = [firstPlace, secondPlace, lastPlace];
+    if(lastPlace == 'Daniel'){
+        posicao.splice(1, 0, 'Daniel')
+        posicao.splice(3, 1)
+    }
+    if(secondPlace == 'Daniel'){
+        posicao.splice(0, 0, 'Daniel')
+        posicao.splice(2, 1)
+    }
+    return posicao
+}
