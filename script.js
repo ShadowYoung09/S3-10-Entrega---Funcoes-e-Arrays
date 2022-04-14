37b8ffe66130a0c5f972ef7b3942ef6268de364b
Henrique Aleixo Mendes <89947763+ShadowYoung09@users.noreply.github.com>
2022-04-14 14:24:25 -0300

    Array & console.log update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function maratonKenzie(firstPlace, secondPlace, lastPlace){
-    const posicao = [firstPlace, secondPlace, lastPlace];
+    const posicao = ['1°-Colocado ', firstPlace, '2°-Colocado ', secondPlace, '3°-Colocado ', lastPlace];
     if(lastPlace == 'Daniel'){
         posicao.splice(1, 0, 'Daniel')
         posicao.splice(3, 1)
@@ -8,9 +8,8 @@ function maratonKenzie(firstPlace, secondPlace, lastPlace){
         posicao.splice(0, 0, 'Daniel')
         posicao.splice(2, 1)
     }
-    return posicao
+    console.log(posicao)
 }
 
-console.log(maratonKenzie('Rafael', 'Daniel', 'Manoel'))
-console.log(maratonKenzie('Rafael', 'Manoel', 'Daniel'))
+
 
